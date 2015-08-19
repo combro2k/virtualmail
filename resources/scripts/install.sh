9e387fd3b87e0f79245d10ba7006cee66d9cc9a3
Combro2k <martijn@vmaurik.nl>
2015-08-19 07:37:32 +0200

    Update install.sh

--- resources/scripts/install.sh
+++ resources/scripts/install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+e () {
+    errcode=$? # save the exit code as the first thing done in the trap function
+    echo "error $errorcode"
+    echo "the command executing at the time of the error was"
+    echo "$BASH_COMMAND"
+    echo "on line ${BASH_LINENO[0]}"
+    # do some error handling, cleanup, logging, notification
+    # $BASH_COMMAND contains the command that was being executed at the time of the trap
+    # ${BASH_LINENO[0]} contains the line number in the script of that command
+    # exit the script or return to try again, etc.
+    exit $errcode  # or use some other value or do return instead
+}
+
+trap e ERR
+
 packages=(
 	'arj'
 	'bitdefender-scanner'
@@ -85,7 +100,7 @@ packages=(
 )
 
 install() {
-    trap 'echo trapped; break' ERR   # Set ERR trap 
+    trap e ERR
     echo '# Setup virtualmail user'
     adduser --system --group --uid 1000 --home /var/vmail --disabled-password vmail
 

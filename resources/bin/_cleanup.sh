b9e88553bb35edbdfbf09595f98c300e027021b3
Combro2k <martijn@vmaurik.nl>
2015-08-22 01:39:10 +0200

    Delete _cleanup.sh

--- resources/bin/_cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-[ -f "/var/run/rsyslogd.pid" ] && rm /var/run/rsyslogd.pid
-[ -d "/var/run/dovecot" ] && [ -f "/var/run/dovecot/master.pid" ] && rm /var/run/dovecot/master.pid
-[ -d "/var/run/amavis" ] && [ -f "/var/run/amavis/amavisd.pid" ] && rm /var/run/amavis/amavisd.pid
